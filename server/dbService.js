e447fd65976a454916e186f14ce1d933169cdd02
oceansalle <oceanesalleiae@gmail.com>
2022-07-07 16:38:42 +0200

    app.js server side

---
+++ server/dbService.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+let instance = null;
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log(err.message);
+    }
+    // console.log('db ' + connection.state);
+});
+
+// Gonna tell if we're connected or not.
+
+
+module.exports = DbService;
